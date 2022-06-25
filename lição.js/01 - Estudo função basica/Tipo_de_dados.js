07e144f7ae32ff9fc0d28de953ff7e136592d2ff
EdCarlosNunes <ed.carlos.git@gmail.com>
2022-06-25 14:57:18 -0300

    Atualizar

--- lição.js/01 - Estudo função basica/Tipo_de_dados.js
+++ lição.js/01 - Estudo função basica/Tipo_de_dados.js
@@ -40,16 +40,16 @@ const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
 //<body>
 //<h2>JavaScript Objects</h2>
 //<p id="demo"></p>
-<script>
-const person = {
-  firstName : "John",
-  lastName  : "Doe",
-  age     : 50,
-  eyeColor  : "blue"
-};
-document.getElementById("demo").innerHTML =
-person.firstName + " is " + person.age + " years old.";
-</script>
+//<script>
+//const person = {
+//  firstName : "John",
+//  lastName  : "Doe",
+//  age     : 50,
+//  eyeColor  : "blue"
+//};
+//document.getElementById("demo").innerHTML =
+//person.firstName + " is " + person.age + " years old.";
+//</script>
 //</body>
 //</html>
 
