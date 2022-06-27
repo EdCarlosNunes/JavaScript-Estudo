37109f87fe387e1da4fa23db8df131b38615b5e8
EdCarlosNunes <ed.carlos.git@gmail.com>
2022-06-27 13:36:21 -0300

    'main'

--- Exercicio/Ex array/ex_pratica_array.js
+++ Exercicio/Ex array/ex_pratica_array.js
@@ -1,16 +1,20 @@
 const hitchedSpaceships = [["fenix", 8, true], ["Golias", 10, true], ["Helmet", 5, false], ["Elemental", 3, true], ["Drawin", 15, false]]
 
-let with9Chars = hitchedSpaceships.filter(element => { return element[0].length >= 7}) 
-console.log(with9Chars[0][0])
+let with9Chars = hitchedSpaceships.filter(element => {
+     return element[1] > 9
+    }).map(element => {
+        return element[0]
+    })
+console.log(with9Chars)
 
 let primeiroTrue = hitchedSpaceships.findIndex((item) => item[2] == true)
 console.log(primeiroTrue)
 
-let upperSpaceship = hitchedSpaceships.map(function(CurrentSpacehip){
-    let upcased = CurrentSpacehip[0].toLocaleUpperCase()
-    return upcased
+let upperSpaceship = hitchedSpaceships.map(spaceshio =>{
+    return spaceshio[0].toLocaleUpperCase()
+    
 })
 console.log(upperSpaceship)
 
-alert("Nave com 9 chars: "+ with9Chars[0][0] +"\nposição da nave com engate pedente: "+ primeiroTrue +"\nNaves: "+ upperSpaceship)
+alert("Nave com +=9 tripulantes: "+ with9Chars[0][0] +"\nposição da nave com engate pedente: "+ (primeiroTrue +1) +"\nNaves: "+ upperSpaceship)
 
