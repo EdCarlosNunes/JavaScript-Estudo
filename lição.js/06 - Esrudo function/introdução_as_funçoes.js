bcd68889d6a5f2f7680d636507523d9ab7111665
EdCarlosNunes <ed.carlos.git@gmail.com>
2022-06-30 16:36:36 -0300

    'main'

--- lição.js/06 - Esrudo function/introdução_as_funçoes.js
+++ lição.js/06 - Esrudo function/introdução_as_funçoes.js
@@ -1,24 +1,40 @@
-//função é um bloco de codigo para executar uma determinada ação
-//esse bloco é nomeado
-//podemos chamar onde e quando precisarmos
-//modelo de uma função
+/*
+Uma função JavaScript é um bloco de código projetado para executar uma tarefa específica.
+Uma função JavaScript é executada quando "algo" a invoca (chama).
+*/
 
 function algumaTarefa(){
-   // <bloco de codigo>
-}
-algumaTarefa()
-
-// exemplo 1
-
-function greetPilot(){ // estamos atribundo um nome a nossa função
-    alert("Bom dai, piloto")// e atribuimos a função alerta
-}
-// porem assim como esta não da alerta nenhum 
-
-//exemplo 2
-
-function greetPilot(){ // estamos atribundo um nome a nossa função
-    alert("Bom dai, piloto")// e atribuimos a função alerta
-}
-greetPilot()//aqui estamos chamando a função
-//nessa o alerta vai aparecer pois chamamos a função 
+    // <bloco de codigo>
+ }
+ algumaTarefa()
+ 
+ // exemplo 1
+ 
+ function greetPilot1(){ // estamos atribundo um nome a nossa função
+     console.log("Bom dai, piloto")// e atribuimos a função alerta
+ }
+ // porem assim como esta não da alerta nenhum 
+ 
+ //exemplo 2
+ 
+ function greetPilot2(){ // estamos atribundo um nome a nossa função
+     console.log("Bom dai, piloto")// e atribuimos a função alerta
+ }
+ greetPilot2()//aqui estamos chamando a função
+ //nessa o alerta vai aparecer pois chamamos a função 
+ 
+ //exemplo 3
+ 
+ function NameFutbol(){
+     let jogador1 = "Neymar"
+ }
+ 
+ let jogadro2 = "cristiano"
+ 
+ console.log("Jogador 1: "+ jogador1 +"\nJogador 2: "+ jogadro2)
+ /*
+ Variáveis ​​locais
+ Variáveis ​​declaradas dentro de uma função JavaScript tornam-se LOCAL para a função.
+ Variáveis ​​locais só podem ser acessadas de dentro da função.
+ */
+ //exemplo 4
